ef378eca9f52d0a234047d3894ac89bda574326c
jajasie-tech <gomjas127@gmail.com>
2026-09-14 16:32:02 +0800

    Create insert_records.sql

---
+++ insert_records.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, name, role) VALUES
+(1, 'Juan Dela Cruz', 'Student'),
+(2, 'Maria Santos', 'Teacher'),
+(3, 'Carlo Reyes', 'Student'),
+(4, 'Angela Cruz', 'Staff'),
+(5, 'Roberto Garcia', 'Admin');
